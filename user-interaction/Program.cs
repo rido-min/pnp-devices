dca6f8ff5b5c752b57c0577202f8066335725432
rido-min <rido-min@users.noreply.github.com>
2021-11-12 14:42:35 -0800

    writable wip

---
+++ user-interaction/Program.cs
@@ -0,0 +1,54 @@
+﻿using Rido.IoTHubClient;
+using System.Text.Json.Nodes;
+using user_interaction;
+
+static string js(object o) => System.Text.Json.JsonSerializer.Serialize(o);
+
+var cs = "HostName=tests.azure-devices.net;DeviceId=usertests;SharedAccessKey=MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDA=";
+var client = await HubMqttClient.CreateAsync(cs);
+Console.WriteLine(client.ConnectionSettings);
+var twinJson = await client.GetTwinAsync();
+
+string? myWritableProp = WritablePropertiesHelper.ReadProperty(nameof(myWritableProp), twinJson);
+
+await client.UpdateTwinAsync(new PropertyAck()
+{
+    Status = 200,
+    Version =  desiredVersion ?? 0,
+    Description = "Updated to default value",
+    Value = js(new { myWritableProp })
+
+}.BuildAck());
+
+twinJson = await client.GetTwinAsync();
+Console.WriteLine(twinJson);
+
+Console.WriteLine("Set value from user");
+myWritableProp = Console.ReadLine();
+
+await client.UpdateTwinAsync(new PropertyAck()
+{
+    Status = 200,
+    Version = desiredVersion ?? 0,
+    Description = "Updated from user value",
+    Value = js(new { myWritableProp })
+
+}.BuildAck());
+
+twinJson = await client.GetTwinAsync();
+Console.WriteLine(twinJson);
+
+client.OnPropertyChange = async p =>
+{
+    System.Console.WriteLine("received property update");
+    return await Task.FromResult(new PropertyAck()
+    {
+        Version = p.Version,
+        Description = "received from service",
+        Status = 200,
+        Value = p.PropertyMessageJson
+
+    });
+};
+
+Console.ReadLine();
---
+++ user-interaction/WritablePropertiesHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace user_interaction
+{
+    internal class WritablePropertiesHelper
+    {
+        internal static string? ReadProperty(string propName, string twinString)
+        {
+            string? prop = null;
+            var twin = JsonNode.Parse(twinString);
+            string? desiredValue = string.Empty;
+            int? desiredVersion = null;
+            string? reportedValue = string.Empty;
+            int? reportedVersion = null;
+
+            if (twin?["desired"]?[propName] != null)
+            {
+                desiredValue = twin?["desired"]?[propName]?.ToString();
+                desiredVersion = Convert.ToInt32(twin?["desired"]?["$version"]?.ToString());
+            }
+
+            if (twin?["reported"]?[propName] != null)
+            {
+                reportedValue = twin?["reported"]?[propName]?["value"]?.ToString();
+                reportedVersion = Convert.ToInt32(twin?["reported"]?[propName]?["av"]?.ToString());
+            }
+
+            if (desiredVersion == reportedVersion)
+            {
+                prop = desiredValue;
+            }
+            else
+            {
+                if (desiredVersion > reportedVersion)
+                {
+                    //should send ack
+
+                }
+            }
+
+            return prop;
+        }
+    }
+}
