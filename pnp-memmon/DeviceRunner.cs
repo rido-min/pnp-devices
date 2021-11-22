44d86da3bfa59e16d8d5bbfdcb6205362a080f5c
rido-min <rido-min@users.noreply.github.com>
2021-11-22 14:26:07 -0800

    first hivemq impl

--- pnp-memmon/DeviceRunner.cs
+++ pnp-memmon/DeviceRunner.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using Rido.IoTHubClient;
 using dtmi_rido_pnp;
 using Humanizer;
+using MQTTnet.Client;
 
 namespace pnp_memmon;
 
@@ -21,10 +22,10 @@ public class DeviceRunner : BackgroundService
     int twinCounter = 0;
     int reconnectCounter = 0;
 
-    dtmi_rido_pnp.memmon? client;
+    dtmi_rido_pnp.memmon_mqtt? client;
 
     const bool default_enabled = true;
-    const int default_interval = 234;
+    const int default_interval = 444;
 
     public DeviceRunner(ILogger<DeviceRunner> logger, IConfiguration configuration)
     {
@@ -35,10 +36,10 @@ public class DeviceRunner : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _logger.LogWarning("Connecting..");
-        client = await dtmi_rido_pnp.memmon.CreateDeviceClientAsync(_configuration.GetConnectionString("hub")) ??
+        client = await dtmi_rido_pnp.memmon_mqtt.CreateDeviceClientAsync(_configuration.GetConnectionString("hive")) ??
             throw new ApplicationException("Error creating MQTT Client");
 
-        client._connection.OnMqttClientDisconnected += (o, e) => reconnectCounter++;
+        client._connection.UseDisconnectedHandler( e => reconnectCounter++);
 
         client.OnProperty_enabled_Updated = Property_enabled_UpdateHandler;
         client.OnProperty_interval_Updated = Property_interval_UpdateHandler;
@@ -48,7 +49,7 @@ public class DeviceRunner : BackgroundService
 
         await client.InitProperty_interval_Async(default_interval);
         await client.InitProperty_enabled_Async(default_enabled);
-
+        
         screenRefresher = new Timer(RefreshScreen, this, 1000, 0);
 
         while (!stoppingToken.IsCancellationRequested)
@@ -71,7 +72,7 @@ public class DeviceRunner : BackgroundService
         {
             Description = "desired notification accepted",
             Status = 200,
-            Version = Convert.ToInt32(req?.Version),
+            //Version = Convert.ToInt32(req?.Version),
             Value = req?.Value
         };
         return await Task.FromResult(ack);
@@ -80,12 +81,33 @@ public class DeviceRunner : BackgroundService
     async Task<WritableProperty<int>> Property_interval_UpdateHandler(WritableProperty<int> req)
     {
         twinCounter++;
+        var description = "property update received";
+        var status = 200;
+        var acceptedValue = client?.Property_interval?.Value;
+        
+        if (client?.Property_enabled?.Value == false)
+        {
+            description = "Rejected, sensor is not enabled";
+            status = 405;
+
+        }
+
+        if (req.Value<0)
+        {
+            description = "Rejected, only positive values";
+            status = 406;
+        } 
+        else
+        {
+            acceptedValue = req?.Value;
+        }
+
         var ack = new WritableProperty<int>("interval")
         {
-            Description = (client?.Property_enabled?.Value == true) ? "desired notification accepted" : "disabled, not accepted",
-            Status = (client?.Property_enabled?.Value == true) ? 200 : 205,
-            Version = Convert.ToInt32(req?.Version),
-            Value = req?.Value ?? 0
+            Description = description,
+            Status = status,
+            //Version = Convert.ToInt32(req?.Version),
+            Value = acceptedValue
         };
         return await Task.FromResult(ack);
     }
@@ -126,8 +148,8 @@ public class DeviceRunner : BackgroundService
             string? interval_value = client?.Property_interval?.Value?.ToString();
             StringBuilder sb = new();
             AppendLineWithPadRight(sb, " ");
-            AppendLineWithPadRight(sb, client?._connection?.ConnectionSettings?.HostName);
-            AppendLineWithPadRight(sb, $"{client?._connection?.ConnectionSettings?.DeviceId} ({client?._connection?.ConnectionSettings?.Auth})");
+            AppendLineWithPadRight(sb, client?.ConnectionSettings?.HostName);
+            AppendLineWithPadRight(sb, $"{client?.ConnectionSettings?.DeviceId} ({client?.ConnectionSettings?.Auth})");
             AppendLineWithPadRight(sb, " ");
             AppendLineWithPadRight(sb, String.Format("{0:8} | {1:5} | {2}", "Property", "Value", "Version"));
             AppendLineWithPadRight(sb, String.Format("{0:8} | {1:5} | {2}", "--------", "-----", "------"));
